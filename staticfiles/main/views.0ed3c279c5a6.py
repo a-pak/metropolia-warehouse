6351892e00b613ed15f7e923b57c5a9a2e625adc
Antti Kuivalainen <124525368+a-pak@users.noreply.github.com>
2024-03-08 11:35:25 +0200

    Delete staticfiles directory

--- staticfiles/main/views.0ed3c279c5a6.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    print('rendering index.html')
-    return render(request, 'index.html')
--- staticfiles/warehouse/production.9d567cdab17e.py
+++
@@ -1,7 +0,0 @@
-from .settings import *
-from .settings import BASE_DIR
-import os
-
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
-
-DEBUG = False
