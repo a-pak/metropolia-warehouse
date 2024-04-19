944d1d46fb865a23966146dbb55f58c95e23572a
a-pak <kuivalainenap@gmail.com>
2024-04-19 11:06:31 +0300

    commit

--- warehouse/settings.py
+++ warehouse/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=i9m&wp-ym6$_@tdbk--g8p0=nwdsupdsz@7+s+808a^klnjl0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://varasto.azurewebsites.net/api/stock/']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', ]
 
 
 # Application definition
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'warehouse.wsgi.application'
 
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
-    
+    "https://varasto.azurewebsites.net",
 ]
 
 DATABASES = {
